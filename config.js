df7b196c693c5eef3be5d6c63a9ebbe5685f29fb
parwati187 <parwatibhandari187@gmail.com>
2022-11-26 14:13:34 +0530

    speech updated

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAb-4iWJPYIXUkpwYPq_d-brvkobCAZFpU",
+  authDomain: "storyapp-c37e6.firebaseapp.com",
+  projectId: "storyapp-c37e6",
+  storageBucket: "storyapp-c37e6.appspot.com",
+  messagingSenderId: "886438294213",
+  appId: "1:886438294213:web:641e41522bf2384ff976ac"
+  };
