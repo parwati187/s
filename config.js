702096984c96319e3e8117a4c683630dc4a0b82b
parwati187 <parwatibhandari187@gmail.com>
2022-11-26 14:45:04 +0530

    config added

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAb-4iWJPYIXUkpwYPq_d-brvkobCAZFpU",
-  authDomain: "storyapp-c37e6.firebaseapp.com",
-  projectId: "storyapp-c37e6",
-  storageBucket: "storyapp-c37e6.appspot.com",
-  messagingSenderId: "886438294213",
-  appId: "1:886438294213:web:641e41522bf2384ff976ac"
+  apiKey: "AIzaSyA97cXxxgi7rt3vkZBZVKUyK7sb6R9ff0Q",
+  authDomain: "storyapp-26-11-2022-a-das.firebaseapp.com",
+  databaseURL: "https://storyapp-26-11-2022-a-das-default-rtdb.firebaseio.com",
+  projectId: "storyapp-26-11-2022-a-das",
+  storageBucket: "storyapp-26-11-2022-a-das.appspot.com",
+  messagingSenderId: "1021707314027",
+  appId: "1:1021707314027:web:33ae0718e494b0b9fca22e"
   };
+
